61de65d742b39adefef8c575af97f68a5d1f254b
gbass787 <4551@holbertonschool.com>
2023-03-25 02:58:45 +0000

    going of the rails

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,12 +5,10 @@ Method that determines if all the boxes can be opened.
 
 
 def canUnlockAll(boxes):
+    if not boxes or not boxes[0]:
+        return True
+
     num_boxes = len(boxes)
-    
-    # return False if there is only one box or no boxes at all
-    if num_boxes <= 1:
-        return True if num_boxes == 1 and boxes[0] == [] else False
-        
     unlocked_boxes = set([0])  # start with the first box unlocked
     new_keys = set(boxes[0])  # keys in the first box
     while new_keys:
