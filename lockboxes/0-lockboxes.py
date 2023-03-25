68066305ce47e13cb622c5921ab5c1cbbf9b72c4
gbass787 <4551@holbertonschool.com>
2023-03-25 03:05:51 +0000

    crazy train

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,9 +5,6 @@ Method that determines if all the boxes can be opened.
 
 
 def canUnlockAll(boxes):
-    if not boxes or not boxes[0]:
-        return True
-
     num_boxes = len(boxes)
     unlocked_boxes = set([0])  # start with the first box unlocked
     new_keys = set(boxes[0])  # keys in the first box
@@ -15,10 +12,14 @@ def canUnlockAll(boxes):
         # Add any new unlocked boxes to the set
         unlocked_boxes |= new_keys
         # Find keys in newly unlocked boxes
-        new_keys = set()
-        for box in unlocked_boxes:
-            new_keys |= set(boxes[box])
+        next_keys = set()
+        for box in new_keys:
+            next_keys |= set(boxes[box])
         # Remove any keys for boxes that have already been unlocked
-        new_keys -= unlocked_boxes
-    # If all boxes are unlocked, return True
-    return len(unlocked_boxes) == num_boxes
+        next_keys -= unlocked_boxes
+        # If all boxes are unlocked, return True
+        if len(unlocked_boxes) == num_boxes:
+            return True
+        new_keys = next_keys
+    # If not all boxes are unlocked, return False
+    return False
