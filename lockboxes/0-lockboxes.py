90c8598dceed538a686c626a0c0aaeca362828d8
gbass787 <4551@holbertonschool.com>
2023-03-25 02:54:41 +0000

    fixing some issues

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,6 +6,11 @@ Method that determines if all the boxes can be opened.
 
 def canUnlockAll(boxes):
     num_boxes = len(boxes)
+    
+    # return False if there is only one box or no boxes at all
+    if num_boxes <= 1:
+        return True if num_boxes == 1 and boxes[0] == [] else False
+        
     unlocked_boxes = set([0])  # start with the first box unlocked
     new_keys = set(boxes[0])  # keys in the first box
     while new_keys:
@@ -18,7 +23,4 @@ def canUnlockAll(boxes):
         # Remove any keys for boxes that have already been unlocked
         new_keys -= unlocked_boxes
     # If all boxes are unlocked, return True
-    if len(unlocked_boxes) == num_boxes:
-        return True
-    else:
-        return False
+    return len(unlocked_boxes) == num_boxes
