01fb9b94f06edc9138a14792b7ae245227f71473
gbass787 <4551@holbertonschool.com>
2023-04-13 04:44:57 +0000

    task 0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+def minOperations(n):
+    if n == 1:
+        return 0
+    
+    ops = [0] * (n + 1)
+    
+    for i in range(2, n + 1):
+        ops[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                ops[i] = min(ops[i], ops[j] + i // j)
+    
+    return ops[n] if ops[n] != float('inf') else 0
