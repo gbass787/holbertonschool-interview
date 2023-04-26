3651d810393c6bca4e42c9f8d56758a0b83405a1
gbass787 <4551@holbertonschool.com>
2023-04-26 04:24:20 +0000

    fix

--- linked_list_palindrome/lists.h
+++ linked_list_palindrome/lists.h
@@ -17,6 +17,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void print_listint(const listint_t *h);
+void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
