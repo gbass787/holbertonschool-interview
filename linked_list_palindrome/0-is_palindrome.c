09772e80ac8be99538fe87a8a1a6350a4f707af4
gbass787 <4551@holbertonschool.com>
2023-04-26 05:02:19 +0000

    major fix

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,40 +1,68 @@
-#include <stdlib.h>
+#include <stddef.h>
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
+ * reverse_list - reverses a linked list
  * @head: pointer to the head of the list
- *
- * Return: 1 if it is a palindrome, 0 otherwise
+ * Return: pointer to the head of the reversed list
+ */
+listint_t *reverse_list(listint_t *head)
+{
+    listint_t *prev = NULL, *current = head, *next = NULL;
+
+    while (current)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double pointer to the head of the list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow, *fast, *prev, *temp;
+    if (!head || !*head || !(*head)->next)
+    {
+        return 1;
+    }
 
-    if (!head || !(*head) || !((*head)->next))
-        return (1);
+    listint_t *slow = *head, *fast = *head, *prev_slow = NULL, *second_half;
 
-    slow = fast = prev = *head;
     while (fast && fast->next)
     {
         fast = fast->next->next;
-        temp = slow;
+        prev_slow = slow;
         slow = slow->next;
-        temp->next = prev;
-        prev = temp;
     }
 
-    if (fast)
-        slow = slow->next;
+    second_half = prev_slow->next;
+    prev_slow->next = NULL;
 
-    while (slow)
+    second_half = reverse_list(second_half);
+
+    listint_t *first_half = *head, *second_half_copy = second_half;
+    int is_palindrome = 1;
+
+    while (first_half && second_half)
     {
-        if (slow->n != prev->n)
-            return (0);
-        slow = slow->next;
-        prev = prev->next;
+        if (first_half->n != second_half->n)
+        {
+            is_palindrome = 0;
+            break;
+        }
+        first_half = first_half->next;
+        second_half = second_half->next;
     }
 
-    return (1);
+    prev_slow->next = reverse_list(second_half_copy);
+
+    return is_palindrome;
 }
 
