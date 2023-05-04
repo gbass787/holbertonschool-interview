e74c500289bdcbc1fddd1f12c548ca2a88f9d566
gbass787 <4551@holbertonschool.com>
2023-05-04 23:49:49 +0000

    task fix

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -4,22 +4,25 @@ import signal
 import re
 from collections import defaultdict
 
+
 def print_statistics(file_size, status_codes):
     print("File size: {}".format(file_size))
     for status_code in sorted(status_codes.keys()):
         print("{}: {}".format(status_code, status_codes[status_code]))
 
+
 def signal_handler(signal, frame):
     print_statistics(total_file_size, status_codes)
     sys.exit(0)
 
-# Register the signal handler for keyboard interruption
+
 signal.signal(signal.SIGINT, signal_handler)
 
 total_file_size = 0
 status_codes = defaultdict(int)
 line_pattern = re.compile(
-    r'^(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] "GET /projects/260 HTTP/1.1" (?P<status_code>\d{3}) (?P<file_size>\d+)$'
+    r'^(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] "GET /projects/260'
+    r' HTTP/1.1" (?P<status_code>\d{3}) (?P<file_size>\d+)$'
 )
 
 line_count = 0
@@ -33,5 +36,4 @@ for line in sys.stdin:
     if line_count % 10 == 0:
         print_statistics(total_file_size, status_codes)
 
-# Print the statistics one last time if the loop has ended
 print_statistics(total_file_size, status_codes)
