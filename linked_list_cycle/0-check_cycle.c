542d7527ca77f4868ff0a7994ee8f837ab42189f
gbass787 <4551@holbertonschool.com>
2023-05-08 05:30:47 +0000

    compiled

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -1,62 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
-
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the head of the list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow_ptr = list;
-    listint_t *fast_ptr = list;
+    listint_t *tortoise, *hare;
 
-    while (slow_ptr && fast_ptr && fast_ptr->next)
+    if (!list)
+        return (0);
+
+    tortoise = list;
+    hare = list;
+
+    while (hare != NULL && hare->next != NULL)
     {
-        slow_ptr = slow_ptr->next;
-        fast_ptr = fast_ptr->next->next;
+        tortoise = tortoise->next;
+        hare = hare->next->next;
 
-        if (slow_ptr == fast_ptr)
-        {
-            return 1;
-        }
+        if (tortoise == hare)
+            return (1);
     }
 
-    return 0;
-}
-
-/* The code below is just for testing purposes and should not be considered part of the solution */
-int main()
-{
-    listint_t *node1 = (listint_t *)malloc(sizeof(listint_t));
-    listint_t *node2 = (listint_t *)malloc(sizeof(listint_t));
-    listint_t *node3 = (listint_t *)malloc(sizeof(listint_t));
-    listint_t *node4 = (listint_t *)malloc(sizeof(listint_t));
-
-    node1->n = 1;
-    node2->n = 2;
-    node3->n = 3;
-    node4->n = 4;
-
-    node1->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    node4->next = NULL; // Set to NULL for no cycle; set to node1 (or any other node) to create a cycle
-
-    if (check_cycle(node1))
-    {
-        printf("The linked list has a cycle.\n");
-    }
-    else
-    {
-        printf("The linked list does not have a cycle.\n");
-    }
-
-    free(node1);
-    free(node2);
-    free(node3);
-    free(node4);
-
-    return 0;
+    return (0);
 }
